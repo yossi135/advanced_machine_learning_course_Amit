fc2703766a273c4a23deb41955cbc111e15b9b29
yossi135 <yossifhendy8@gmial.com>
2024-10-06 19:54:19 +0300

    tesst

---
+++ matrial/session/test.py
@@ -0,0 +1,3 @@
+my_name='youssif'
+reverse_my_name=my_name[0:0:-1]
+print(reverse_my_name)
---
+++ matrial/task/task1.py
@@ -0,0 +1,24 @@
+
+email=input('''please inter your email and contains to 17 (charaters)or(numbers)
+          such as(username@domain.domain ending) :  ''')
+if len(email)==17:
+    
+  count=str(email.count('@'))
+  if '.' in email and  count=='1':
+     print(f'this email ({email}) is valid ')
+     username=email[0:7]
+     domain=email[8:13]
+     domain_ending=email[13:17]
+
+     print(f'username is  ****{username}***')
+     print(f'domain is  ***{domain}***')
+    
+     if domain_ending=='.com':
+        print(f'The domain ending is  ****{domain_ending}****')     
+     else:
+        print(f'do not know this  ****{domain_ending}****')               
+  else:
+   print(f'this email ({email}) is invalid')    
+else:
+    print('error!! please try again')   
+     
