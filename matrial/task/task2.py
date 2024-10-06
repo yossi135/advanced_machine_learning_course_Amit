bebe16b16b37394a5e69ca136b9bd6be2a57ab89
yossi135 <yossifhendy8@gmial.com>
2024-10-06 23:20:00 +0300

    i put the new word in a variable (replace) in line 7

--- matrial/task/task2.py
+++ matrial/task/task2.py
@@ -4,5 +4,5 @@ print(f'the core part of message is  ****{the_core_part}****')
 reverse=the_core_part[ : :-1]
 print(f'the first word becomes {reverse}')
 new_word=Encoded_Message[14:19]
-new_word.replace('E','')
-print(f'the final message is {reverse} {new_word}')
+replace=new_word.replace('E','')
+print(f'the final message is {reverse} {replace}')
