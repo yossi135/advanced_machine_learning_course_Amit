8096a8ca9b07049645f265397461383dfb90c28e
yossi135 <yossifhendy8@gmail.com>
2024-10-20 14:40:45 +0300

    convert to class multiplication

--- matrial/code/similar str.py
+++ matrial/code/similar str.py
@@ -1,12 +1,14 @@
 str1=input().split(' ')
 str2=input().split(' ')
 list=[]
+
 class similar_str:
     def __init__(self,str1,str2):
         self.str1=str1
         self.str2=str2
        
     def give(self):
+        
        for i in self.str1:    
          for j in self.str2:
             if j==i:
